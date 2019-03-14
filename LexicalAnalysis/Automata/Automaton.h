ace2b8c732cba6445b55bb0b09f4ae067eec799b
Hesham Medhat <heshammedhat5@gmail.com>
2019-03-14 22:00:58 +0200

    Add automata skeleton of State and Automaton classes

---
+++ LexicalAnalysis/Automata/Automaton.h
@@ -0,0 +1,15 @@
+#ifndef OUR_JAVA_COMPILER_AUTOMATON_H
+#define OUR_JAVA_COMPILER_AUTOMATON_H
+
+
+#include "State.h"
+
+class Automaton {
+
+private:
+    State startState;
+    State endState;
+};
+
+
+#endif
---
+++ LexicalAnalysis/Automata/State.h
@@ -0,0 +1,10 @@
+#ifndef OUR_JAVA_COMPILER_NODE_H
+#define OUR_JAVA_COMPILER_NODE_H
+
+
+class State {
+
+};
+
+
+#endif
