de3e59bfcf71b153b5ed7f63bc521a80bdfce729
Hesham Medhat <heshammedhat5@gmail.com>
2019-03-17 22:12:01 +0200

    Use STL map for NFA transitions DS instead and remove unused import in State

--- LexicalAnalysis/Automata/NFAState.h
+++ LexicalAnalysis/Automata/NFAState.h
@@ -2,13 +2,14 @@
 #define OUR_JAVA_COMPILER_NFASTATE_H
 
 
+#include <map>
 #include "State.h"
 
 class NFAState : public State {
 public:
     NFAState(Token& acceptedToken);
 private:
-    std::unordered_map<char, std::unordered_set<State&>> transitions;
+    std::map<char, std::unordered_set<State&>> transitions;
 };
 
 
--- LexicalAnalysis/Automata/State.h
+++ LexicalAnalysis/Automata/State.h
@@ -3,7 +3,6 @@
 
 
 #include <unordered_map>
-#include <set>
 #include <unordered_set>
 
 #include "../Token.h"
