d9a38c4a2e44d4ac4c04e5ae167e84904a4d2c7e
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2019-04-08 01:23:22 +0200

    Polish main.c

--- main.cpp
+++ main.cpp
@@ -5,26 +5,23 @@
 #include "LexicalAnalysis/LexicalAnalyzerGenerator.h"
 #include "LexicalAnalysis/LexicalAnalyzer.h"
 
-int main() {
-    std::cout << "Rules input file relative path: " << std::endl;
+int main(int argc, char* argv[]) {
+  if (argc < 3) return 1;
 
-    std::string inputRulesFilePath="C:\\Users\\Ahmed\\Desktop\\text.txt";
-   // std::cin >> inputRulesFilePath; // Read input file path
-    std::ifstream inputRulesStream(inputRulesFilePath);
+  std::ifstream rulesFile(argv[1]);
+  if (!rulesFile) {
+    std::cout << "Could not open " << argv[1] << std::endl;
+    return 1;
+  }
 
-    std::cout << "Program file relative path: " << std::endl;
+  std::cout << "Initializing Lexical Analyzer Generator..." << std::endl;
+  LexicalAnalyzerGenerator lexicalAnalyzerGenerator(rulesFile);
+  std::cout << "Generating Lexical Analyzer..." << std::endl;
+  LexicalAnalyzer lexicalAnalyzer
+    = lexicalAnalyzerGenerator.buildLexicalAnalyzer();
 
-    std::string programFilePath="C:\\Users\\Ahmed\\Desktop\\text2.txt";
- //   std::cin >> programFilePath; // Read input file path
+  std::cout << "Analyzing Program..." << std::endl;
+  lexicalAnalyzer.analyzeCompleteProgram(argv[2]);
 
-    // Build lexical analyzer
-    std::cout << "Starting Lexical Analyzer Generator..." << std::endl;
-    LexicalAnalyzerGenerator lexicalAnalyzerGenerator(inputRulesStream);
-    std::cout << "Generating Lexical Analyzer..." << std::endl;
-    LexicalAnalyzer lexicalAnalyzer = lexicalAnalyzerGenerator.buildLexicalAnalyzer();
-
-    std::cout << "Analyzing Program..." << std::endl;
-    lexicalAnalyzer.analyzeCompleteProgram(programFilePath);
-
-    return 0;
+  return 0;
 }
