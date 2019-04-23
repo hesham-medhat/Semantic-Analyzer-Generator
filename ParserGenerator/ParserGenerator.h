6aa4bc834d1077ae55e107991d60d4747ffa88b6
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2019-04-23 06:06:41 +0200

    Implement generateParser

--- ParserGenerator/ParserGenerator.h
+++ ParserGenerator/ParserGenerator.h
@@ -9,9 +9,28 @@
  * stream of grammar rules and their lexical analyzer */
 class ParserGenerator {
 public:
-    static Parser generateParser(std::istream&, LexicalAnalyzer&);
+    /// Generates LL(1) parser using rules from given input stream.
+    ///
+    /// @param  rulesIstream  stream to read rules from
+    /// @param  lex           LexicalAnalyzer to use in generated parser
+    ///
+    /// @return  generated Parser instance
+    static Parser generateParser(std::istream& rulesIstream,
+                                 LexicalAnalyzer& lex);
 private:
     ParserGenerator(); // Not implemented on purpose
+    static constexpr char eof = std::string::traits_type::eof();
+    static const std::string reserved;
+    static bool skip(std::istream&, const std::string& = "");
+    static std::string getUntil(std::istream&, const std::string&);
+    static GrammarSymbol::Production getProduction(
+        std::istream&,
+        std::unordered_map<std::string, TerminalSymbol::ptr>&,
+        std::unordered_map<std::string, NonTerminalSymbol::ptr>&);
+    static inline bool isValidSymbolName(const std::string&);
+    static inline bool contains(const std::string&, const char&);
+    template<typename K, typename V>
+    static inline bool contains(const std::unordered_map<K, V>&, const K&);
 };
 
 
