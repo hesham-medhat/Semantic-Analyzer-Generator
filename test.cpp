85eee36e6c6d792af47241a275af7fa99fdf8f79
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2019-04-23 23:23:18 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -20,5 +20,41 @@ int main(int argc, char* argv[]) {
   LexicalAnalyzer dummyLex = LexicalAnalyzer(Automaton());
   Parser parser = ParserGenerator::generateParser(rulesFile, dummyLex);
 
+  std::ofstream otmp("tmpfile");
+  parser.save(otmp);
+  std::ifstream itmp("tmpfile");
+  Parser loadedParser(dummyLex, itmp);
+
+  for (const auto& symbol : parser.nonTerminals) {
+    for (const auto& prod : symbol.second->productions) {
+      std::cout << symbol.first << " ::= ";
+      for (const auto& rhsTerm : prod) {
+        if (rhsTerm->getType() == GrammarSymbol::Terminal) {
+          std::cout << "'" << rhsTerm->getName() << "' ";
+        } else {
+          std::cout << rhsTerm->getName() << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
+  std::cout << "LOADED PARSER:\n" << std::endl;
+  for (const auto& symbol : parser.nonTerminals) {
+    for (const auto& prod : symbol.second->productions) {
+      std::cout << symbol.first << " ::= ";
+      for (const auto& rhsTerm : prod) {
+        if (rhsTerm->getType() == GrammarSymbol::Terminal) {
+          std::cout << "'" << rhsTerm->getName() << "' ";
+        } else {
+          std::cout << rhsTerm->getName() << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
+
   return 0;
 }
