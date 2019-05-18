c6d5891d0cb52ae3e18fdc0d34ff69ef7bfe92c6
Abdelhakeem <abdelhakeem.osama@hotmail.com>
2019-05-19 00:29:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "ParserGenerator/ParserGenerator.h"
 
 int main(int argc, char* argv[]) {
+  constexpr auto SAVED_LEXER_FNAME = "pgen.lexer";
+  constexpr auto SAVED_PARSER_FNAME = "pgen.parser";
+
   if (argc != 3) {
     std::cout << "USAGE: "
               << basename(argv[0])
@@ -27,12 +30,34 @@ int main(int argc, char* argv[]) {
   }
 
   LexicalAnalyzerGenerator lexGen(lexerRulesFile);
+  std::cout << "Generating lexical analyzer..." << std::endl;
   LexicalAnalyzer lex = lexGen.buildLexicalAnalyzer();
-  Parser parser = ParserGenerator::generateParser(parserRulesFile, lex);
-  std::ofstream offf("pgen.parser");
-  parser.save(offf);
 
-  // TODO: write parser source code to output file
+  std::cout << "Generating parser and semantic analyzer..." << std::endl;
+  Parser parser = ParserGenerator::generateParser(parserRulesFile, lex);
+  std::cout << "Parser implementation written to pgen.parser.cpp" << std::endl;
+
+  std::ofstream savedLexer(SAVED_LEXER_FNAME);
+  if (!savedLexer) {
+    std::cerr << "Cannot open file to write generated lexer: "
+              << SAVED_LEXER_FNAME << std::endl;
+    return 1;
+  }
+
+  std::ofstream savedParser(SAVED_PARSER_FNAME);
+  if (!savedParser) {
+    std::cerr << "Cannot open file to write generated parser: "
+              << SAVED_PARSER_FNAME << std::endl;
+    return 1;
+  }
+
+  std::cout << "Saving generated lexer to " << SAVED_LEXER_FNAME << "..."
+            << std::endl;
+  lex.saveLexicalAnalyzerAutomaton(savedLexer);
+
+  std::cout << "Saving generated parser to " << SAVED_PARSER_FNAME << "..."
+            << std::endl;
+  parser.save(savedParser);
 
   return 0;
 }
